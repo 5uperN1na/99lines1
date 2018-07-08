02031be99f05d9ee7c9a55063b67fae9b8c61b9d
Paula Suarez <pauladzuck@hotmail.com>
2018-07-08 15:28:31 -0500

    Adding outer and inner for loops to app.js file.

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+//99 lines of code in the file, 99 lines of code; John strikes one out, clears it all out, 
+//98 lines of code in the file, 98 lines of code; John strikes one out, clears it all out, 
+//97 lines of code in the file
+
+var i;
+var j;
+
+var friends = ['Erica', 'David', 'Sharon', 'Hernan', 'Olivia'];
+for (j = 0; j < friends.length; j++) {
+  console.log(friends[j]);
+
+for (i = 100; i > 0; i--) {
+   console.log(i);
+}
+};
+
