1133acabd96e2d5ec50f4cc1509445205400be36
Paula Suarez <pauladzuck@hotmail.com>
2018-07-08 21:21:19 -0500

    Added changes to app.js file (conditional statement).

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ var j;
 
 var friends = ['Erica', 'David', 'Sharon', 'Hernan', 'Olivia'];
 for (j = 0; j < friends.length; j++) {
-  console.log(friends[j]);
+    console.log(friends[j].toUpperCase() + ':');
 
-for (i = 100; i > 0; i--) {
-   console.log(i);
-}
+    for (i = 100; i > 0; i--) {
+
+        if (i > 1) {
+            console.log(i + ' lines of code in a file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ' + i + ' lines of code in a file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ');
+        }
+        else {
+            console.log(i + ' line of code in a file, ' + i + ' line of code; ' + friends[j] + ' strikes one out, clears it all out, no more lines of code in the file. ');
+        }
+    }
+
+
 };
 
