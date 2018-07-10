5132a234a62173331742a891530973f5df124aa9
Paula Suarez <pauladzuck@hotmail.com>
2018-07-10 13:20:26 -0500

    added final updates to for loops in app.js and html/css updates to index and styles files.

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 
 
-//99 lines of code in the file, 99 lines of code; John strikes one out, clears it all out, 
-//98 lines of code in the file, 98 lines of code; John strikes one out, clears it all out, 
-//97 lines of code in the file
 
 var i;
 var j;
+var text = '';
 
-var friends = ['Erica', 'David', 'Sharon', 'Hernan', 'Olivia'];
+var friends = ['Erica', 'David', 'Meredith', 'Hernan', 'Ashley'];
 for (j = 0; j < friends.length; j++) {
-    console.log(friends[j].toUpperCase() + ':');
+    text += friends[j].toUpperCase() + ':\n';
 
-    for (i = 100; i > 0; i--) {
+    for (i = 99; i > 0; i--) {
 
         if (i > 1) {
-            console.log(i + ' lines of code in a file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ' + i + ' lines of code in a file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, ');
+            text += i + ' lines of code in a file, ' + i + ' lines of code; ' + friends[j] + ' strikes one out, clears it all out, \n';
         }
         else {
-            console.log(i + ' line of code in a file, ' + i + ' line of code; ' + friends[j] + ' strikes one out, clears it all out, no more lines of code in the file. ');
+             text += i + ' line of code in a file, ' + i + ' line of code; ' + friends[j] + ' strikes one out, clears it all out, no more lines of code in the file. \n';
         }
     }
-
-
-};
+}
+console.log(text)
 
